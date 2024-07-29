8855a7dd1efb29de61b23ac5def96b616f562bba
TimisRobert <roberttimis@proton.me>
2024-07-29 22:33:48 +0200

    chore: change license

---
+++ devenv.nix
@@ -0,0 +1,12 @@
+{
+  config,
+  pkgs,
+  ...
+}: {
+  env.MIX_HOME = "${config.env.DEVENV_STATE}/.mix";
+
+  packages = [
+    pkgs.beam.packages.erlang_27.elixir_1_17
+    pkgs.mosquitto
+  ];
+}
--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ControlPacket.MixProject do
   def project do
     [
       app: :control_packet,
-      version: "1.1.0",
+      version: "1.1.1",
       elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
@@ -33,7 +33,7 @@ defmodule ControlPacket.MixProject do
 
   defp package() do
     [
-      licenses: ["AGPL-3.0-or-later"],
+      licenses: ["MIT"],
       links: %{"GitHub" => "https://github.com/TimisRobert/control_packet"}
     ]
   end
